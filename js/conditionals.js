c3667de403f2587666c015117e6bfc19112f1658
Scott McCrindle <scott@mccrindle.me>
2024-01-31 10:42:44 -0500

    Improved the conditionals js file

--- js/conditionals.js
+++ js/conditionals.js
@@ -25,6 +25,7 @@ function setWeather() {
 	console.log("setWeather function called!");
 	let choice = select.value;
 	let temperature = temp.value;
+	console.log(`Weather is ${choice} and temperature is ${temperature}.`);
 	/* STEP 2b: Craft an IF/ELSEIF/ELSE that changes the src attribute of the icon element to the appropriate .svg file in the images folder */
 
 	/* STEP 3: Add a nested IF/ELSE statement inside the 'sunny' condition above that tests for temperature, and if it is equal to or greater than 15, turn the page background orange, otherwise turn it lightblue */
